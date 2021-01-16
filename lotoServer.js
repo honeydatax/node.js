78dd601a5aace4112a62d0409c25539621269451
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-01-16 16:29:46 +0000

    http://127.0.0.1:8090/

---
+++ lotoServer.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var s="";
+var i=0;
+var ii=0;
+http.createServer(function (req, res) {
+   console.log(req.url.toString());
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   s="";
+   for (i=0;i<10;i++){
+       ii=Math.floor(Math.random()*49+1);
+       s=s + ii.toString()+"\r\n";
+   }
+   res.end(s+"\r\n");
+}).listen(8090);
+
+console.log('request server');
