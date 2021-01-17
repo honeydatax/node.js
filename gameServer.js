e89685c43a49918eea0a4d8f80db53651febd19c
lisbon <16432726+honeydatax@users.noreply.github.com>
2021-01-17 15:09:46 +0000

    curl 127.0.0.1:8090

--- gameServer.js
+++ gameServer.js
@@ -4,20 +4,33 @@ var i=0;
 var ii=0;
 var iii=0;
 var iiii=0;
+var aaa=Array(16*22);
 var map="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   s="\r\n\r\n";
-   for (i=0;i<20;i++){
-	for(iii=0;iii<16;iii++){
-	       ii=Math.floor(Math.random()*23);
-       	       s=s + map[ii].toString();
+   for (i=0;i<16;i++){
+	for(iii=0;iii<20;iii++){
+	       aaa[22*i+iii]=' ';
 	}
 	s=s +"\r\n";
    }
+   for (i=0;i<16;i++){
+	       aaa[i*22+20]='\r';
+	       aaa[i*22+21]='\n';
+
+	s=s +"\r\n";
+   }
+
+http.createServer(function (req, res) {
+   res.writeHead(200, {'Content-Type': 'text/plain'});
+   for (i=0;i<16;i++){
+	for(iii=0;iii<20;iii++){
+	       ii=Math.floor(Math.random()*23);
+       	       aaa[i*22+iii]=map[ii];
+	}
+
+   }
+   s=aaa.join();
+   s=s.replace(/,/gi,"");
    res.end(s);
-   console.log(iiii.toString());
-   iiii=iiii+1;
 }).listen(8090);
 
 console.log('request server');
